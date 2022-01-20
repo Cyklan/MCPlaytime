1712802b353d77081f6c60959f128cd4b2c5f1a9
Cyklan <37187056+Cyklan@users.noreply.github.com>
2022-01-20 20:27:09 +0100

    Position options

--- src/main/java/de/cyklan/mctimer/GUI/ConfigScreen.java
+++ src/main/java/de/cyklan/mctimer/GUI/ConfigScreen.java
@@ -2,12 +2,15 @@ package de.cyklan.mctimer.GUI;
 
 import de.cyklan.mctimer.Timer;
 import de.cyklan.mctimer.util.Config;
+import de.cyklan.mctimer.util.HorizontalPosition;
 import de.cyklan.mctimer.util.Loader;
+import de.cyklan.mctimer.util.VerticalPosition;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
 import me.shedaniel.clothconfig2.gui.entries.ColorEntry;
+import me.shedaniel.clothconfig2.gui.entries.EnumListEntry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.LiteralText;
@@ -20,12 +23,14 @@ public class ConfigScreen {
         ConfigBuilder builder = this.createConfigBuilder();
         ConfigCategory colorCategory = this.createCategory(builder, "Color");
         ConfigCategory displayCategory = this.createCategory(builder, "Display");
+        ConfigCategory positionCategory = this.createCategory(builder, "Position");
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
         colorCategory.addEntry(this.colorOption(entryBuilder));
         displayCategory.addEntry(this.hoursOption(entryBuilder));
         displayCategory.addEntry(this.millisOption(entryBuilder));
-
+        positionCategory.addEntry(this.horizontalOption(entryBuilder));
+        positionCategory.addEntry(this.verticalOption(entryBuilder));
 
         Screen screen = builder.build();
         MinecraftClient.getInstance().setScreen(screen);
@@ -67,6 +72,27 @@ public class ConfigScreen {
                 .setDefaultValue(config.getRgbColor())
                 .setSaveConsumer((value) -> config.setRgbColor(value))
                 .build();
+    }
 
+    private EnumListEntry horizontalOption(ConfigEntryBuilder builder) {
+        return builder.startEnumSelector(
+                    new LiteralText("Horizontal Position"),
+                    HorizontalPosition.class,
+                    config.getHorizontalPosition()
+                )
+                .setDefaultValue(config.getHorizontalPosition())
+                .setSaveConsumer((value) -> config.setHorizontalPosition(value))
+                .build();
+    }
+
+    private EnumListEntry verticalOption(ConfigEntryBuilder builder) {
+        return builder.startEnumSelector(
+                        new LiteralText("Vertical Position"),
+                        VerticalPosition.class,
+                        config.getVerticalPosition()
+                )
+                .setDefaultValue(config.getVerticalPosition())
+                .setSaveConsumer((value) -> config.setVerticalPosition(value))
+                .build();
     }
 }
